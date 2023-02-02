85729936cceb4ec1fdf519f09ed22ff73caae40b
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-02 16:03:10 +0530

    making ours views and using inbult user signup form and registration form

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+# from . forms 
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
